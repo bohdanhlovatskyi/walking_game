9015a7662c60dc3522d505cb10f9a4bd3206a7c2
bohdanhlovatskyi <bohdan.hlovatskyi@ucu.edu.ua>
2021-03-03 18:24:34 +0200

    Debugged, workd fine with given script

--- game.py
+++ game.py
@@ -1,58 +1,75 @@
 '''
- В модулях повинні бути реалізовані наступні класи: Кімната, Персонаж, Ворог, Товариш, Предмет.
-Протокол (файл log.txt) одного сеансу гри також може допомогти успішно виконати це завдання.
+The following classes are implemented in the module:
+Room, Character, Enemy, Friend, Subject.
+These are used for implementation of simple walking game.
 '''
 
 from typing import List, Type, Dict, Optional
 
-current_room = ''
-
 class Room:
-    def __init__(self, name: str,
-                characters: List[Type['Character']]=[],
-                items: List[Type['Item']]=[],
-                directions: Dict[str, str]={}):
+    """
+    Room can contain enemies, items and is linked to other rooms
+    """
+
+    def __init__(self, name: str) -> 'Room':
         self.name = name
-        self.character = None
-        self.item = None
-        self.directions = directions
-        self.description = None
+        self.character = self.item = self.description = None
+        self.directions = {}
 
     def set_description(self, message: str): 
-        self.description = str(message)
+        """Adds a description to a room
+        Args:
+            message (str)
+        """
+
+        self.description = message
 
     def set_character(self, character: Type['Chatacter']):
+        """ Adds an enemy to a room
+
+        Args:
+            character (Type['Character'])
+        """
+
         self.character = character
 
-    def get_character(self):
-        """ As far as I see, the rule is that each room has only one character
-
-        Returns:
-            [type]: [description]
+    def get_character(self) -> Type['Enemy']:
         """
+        Returns an Enemy instance
+        """
+
         return self.character
 
     def set_item(self, item: Type['Item']):
+        """setter"""
+
         self.item = item
 
-    def get_item(self):
+    def get_item(self) -> Type['Item']:
         return self.item
 
     def link_room(self, other_room: Type['Room'], direction: str):
         self.directions[direction] = other_room
 
     def get_details(self):
-        for key in self.__dict__:
-            print(self.__dict__[key])
+        print(self.name)
+        print('--------------------')
+        print(self.description)
+        for direction in self.directions:
+            try:
+                print(f'The {self.directions[direction].name} is {direction}')
+            except KeyError: continue
 
     def move(self, direction: str):
         try:
             return self.directions[direction]
         except KeyError:
-            return current_room
+            print(f'The entrance to {direction} direction is covered with stones...')
+            return self
 
     def __repr__(self):
         return f'Room({self.name}, {self.description}, {self.character}, {self.item})'
+
 
 class Item:
     def __init__(self, name: str):
@@ -66,15 +83,14 @@ class Item:
         return self.name
 
     def describe(self):
-        return self.description
+        print(f'The {self.name} is here - {self.description}')
+
 
 class Character:
-    def __init__(self, name: str, description: str='', lives: int=3):
+    def __init__(self, name: str, description: str=''):
         self.name = name
         self.description = description
-        self.lives = lives
-        self.message = None
-        self.weakness = None
+        self.message = self.weakness = None
 
     def set_conversation(self, message: str):
         self.message = message
@@ -82,50 +98,26 @@ class Character:
     def set_weakness(self, weakness: str):
         self.weakness = weakness
 
-
     def describe(self):
-        return self.description
+        print(f'{self.name} is here!')
+        print(f'{self.description}')
 
     def talk(self):
-        print(self.message)
+        print(f'{self.name} says: {self.message}')
 
-class Hero(Character):
-    def __init__(self, lives: int=3, 
-                backpack: List[Type['Item']]=[]):
-        super().__init__(lives=lives)
-        self.backpack = backpack
 
 class Enemy(Character):
+
     defeated: int = 0
     def __init__(self, name: str, description: str):
         super().__init__(name, description)
 
-
-    def fight(self, fight_with: Optional[Type['Item']]=None) -> bool:
+    def fight(self, fight_with: str=None) -> bool:
         if fight_with == self.weakness:
-            self.defeated += 1
+            Enemy.defeated += 1
             return True
 
         return False
 
     def get_defeated(self):
         return self.defeated
-
-
-class Friend(Character):
-    def __init__(self, mame: str, description: str,
-                item_to_give: Type['Item'], lives: int=3):
-        super().__init__(name, description, lives=lives)
-        self.item_to_give = item_to_give
-
-class GameField:
-    def __init__(self, rooms: Optional[List[Type['Room']]]=None,
-                    current_room: Optional[Type['Room']]=None):
-        self.rooms = rooms
-        self.current_room = current_room
-
-    def add_room(self, room: Type['Room']):
-        self.rooms.append(room)
-
-    def set_current_room(self, room: Type['Room']):
-        self.current_room = current_room
